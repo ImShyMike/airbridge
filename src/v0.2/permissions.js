a985d066e478a30a77c514ffb093da6cc019b01b
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2026-07-08 00:20:06 +0100

    fix: lookup tokens using airtable instead of v0.1

--- src/v0.2/permissions.js
+++ src/v0.2/permissions.js
@@ -1,20 +1,30 @@
 import fs from "fs"
 import path from "path"
 import yaml from "js-yaml"
+import Airtable from "airtable"
+
+const AIRBRIDGE_BASE_ID = "appP3uDe6tFt7cA5r"
+const AUTHTOKENS_TABLE = "Authtokens"
+let authtokensTable
+
+function getAuthtokensTable() {
+  if (!authtokensTable) {
+    authtokensTable = new Airtable({
+      apiKey: process.env.AIRTABLE_API_KEY,
+    }).base(AIRBRIDGE_BASE_ID)(AUTHTOKENS_TABLE)
+  }
+
+  return authtokensTable
+}
 
 export async function getPermissions(authId) {
   const sanitizedAuthId = String(authId).replace(/'/g, "\\'")
-  const opts = {
-    maxRecords: 1,
-    filterByFormula: `Authtoken='${sanitizedAuthId}'`,
-  }
-
-  const records = await (
-    await fetch(
-      "https://airbridge.hackclub.com/v0.1/Airbridge/Authtokens?select=" +
-        encodeURIComponent(JSON.stringify(opts))
-    )
-  ).json()
+  const records = await getAuthtokensTable()
+    .select({
+      maxRecords: 1,
+      filterByFormula: `Authtoken='${sanitizedAuthId}'`,
+    })
+    .all()
   const record = records[0]
   if (!record) {
     return null
